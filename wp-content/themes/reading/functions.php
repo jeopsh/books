fb93266acedcabb03bafb2e75d5e4a40b6eaec15
Joseph Chan <jeopsh@gmail.com>
2018-04-16 16:42:38 +0800

    用户入口

--- wp-content/themes/reading/functions.php
+++ wp-content/themes/reading/functions.php
@@ -16,7 +16,9 @@ function my_theme_enqueue_styles() {
 	);
 }
 
-/* Page Navigation */
+/**
+ * Page Navigation
+ */
 function numeric_paging_nav() {
 	global $wp_query;
 
@@ -30,15 +32,115 @@ function numeric_paging_nav() {
 
 			<?php wp_pagenavi(); ?>
 
-			<!--			--><?php //if ( get_next_posts_link() ) : ?>
-			<!--			<div class="nav-previous">--><?php //next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentythirteen' ) ); ?><!--</div>-->
-			<!--			--><?php //endif; ?>
-			<!---->
-			<!--			--><?php //if ( get_previous_posts_link() ) : ?>
-			<!--			<div class="nav-next">--><?php //previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?><!--</div>-->
-			<!--			--><?php //endif; ?>
-
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->
 	<?php
+}
+
+/**
+ * Include Custom JavaScript
+ */
+add_action('wp_enqueue_scripts', 'load_my_scripts');
+
+function load_my_scripts(){
+	wp_enqueue_script('jquery');
+
+	wp_enqueue_script('my_second_script', get_stylesheet_directory_uri() . '/js/main.js');
+}
+
+
+/**
+ * Redirect Login To Home Page
+ */
+function my_login_redirect($redirect_to, $request){
+	if( empty( $redirect_to ) || $redirect_to == 'wp-admin/' || $redirect_to == admin_url() )
+		return home_url("/wp-admin/edit.php");
+	else
+		return $redirect_to = 'http://localhost/books';
+}
+add_filter("login_redirect", "my_login_redirect", 10, 3);
+
+
+/**
+ * Custom Login Page
+ */
+
+// Login Logo
+function custom_login_logo() {
+	echo '<style type="text/css">
+        .login h1 a {
+            background-image:url("http://localhost/books/wp-content/themes/reading/images/Logo-Login.png") !important;
+        }
+    </style>';
+}
+add_action('login_head', 'custom_login_logo');
+
+// Login Logo URL
+function custom_loginlogo_url($url) {
+	return get_bloginfo('url'); //修改URL地址
+}
+add_filter( 'login_headerurl', 'custom_loginlogo_url' );
+
+//Login Logo Description
+function custom_loginlogo_desc($url) {
+	return '新语图书'; //修改文本信息
+}
+add_filter( 'login_headertitle', 'custom_loginlogo_desc' );
+
+// Lode The style.css
+function custom_login() {
+	echo '<link rel="stylesheet" type="text/css" href="http://localhost/books/wp-content/themes/reading/css/login.css" />';
+}
+add_action('login_head', 'custom_login');
+
+
+/**
+ * Custom The Back End User List Page
+ */
+add_filter('manage_users_columns', 'add_user_nickname_column');
+function add_user_nickname_column($columns) {
+	$columns['user_nickname'] = '昵称';
+	unset($columns['name']);
+	return $columns;
+}
+
+add_action('manage_users_custom_column',  'show_user_nickname_column_content', 20, 3);
+function show_user_nickname_column_content($value, $column_name, $user_id) {
+	$user = get_userdata( $user_id );
+	$user_nickname = $user->nickname;
+	if ( 'user_nickname' == $column_name )
+		return $user_nickname;
+	return $value;
+}
+
+
+/**
+ * Custom The Back End User Info Edit Page
+ */
+add_action('show_user_profile','wpjam_edit_user_profile');
+add_action('edit_user_profile','wpjam_edit_user_profile');
+function wpjam_edit_user_profile($user){
+	?>
+    <script>
+        jQuery(document).ready(function($) {
+            $('#first_name').parent().parent().hide();
+            $('#last_name').parent().parent().hide();
+            $('#display_name').parent().parent().hide();
+            $('.show-admin-bar').hide();
+        });
+    </script>
+	<?php
+}
+
+// Focused On User Nickname
+add_action('personal_options_update','wpjam_edit_user_profile_update');
+add_action('edit_user_profile_update','wpjam_edit_user_profile_update');
+function wpjam_edit_user_profile_update($user_id){
+	if (!current_user_can('edit_user', $user_id))
+		return false;
+	$user = get_userdata($user_id);
+	$_POST['nickname'] = ($_POST['nickname'])?:$user->user_login;
+	$_POST['display_name']	= $_POST['nickname'];
+	$_POST['first_name']	= '';
+	$_POST['last_name']		= '';
 }
--- wp-content/themes/reading/header.php
+++ wp-content/themes/reading/header.php
@@ -29,15 +29,32 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+            $(".user-icon").hover(function() {
+                $(".user-menu").css("display","block");
+            }, function() {
+                $(".user-menu").css("display","none");
+            });
+            $(".user-menu").hover(function() {
+                $(this).css('display','block');
+            }, function() {
+                $(this).css('display','none')
+            })
+        });
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
 			<div class="home-link">
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-                    <h1 class="site-title"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/Logo.png"><?php bloginfo( 'name' ); ?></h1>
-                </a>
+                    <h1 class="site-title">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/Logo.png"><?php bloginfo( 'name' ); ?>
+                        </a>
+                    </h1>
             <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</div>
 
@@ -47,6 +64,48 @@
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
 					<?php get_search_form(); ?>
+                    <div class="user">
+                        <div class="user-icon"></div>
+                        <div class="user-menu">
+                            <div class="user-info">
+                                <a href="http://localhost/books/wp-admin/profile.php">
+                                    <div class="user-img">
+                                        <?php global $current_user; wp_get_current_user(); echo get_avatar( $current_user->ID, 64 ); ?>
+                                    </div>
+                                    <div class="user-name">
+                                            <?php echo $current_user->display_name.'<br>'; echo $current_user->user_login; ?>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="user-action">
+	                            <?php if ( is_user_logged_in() ) : ?>
+		                            <?php if ( in_array('administrator', $current_user->roles)) : ?>
+                                        <p><a href="http://localhost/books/wp-admin/post-new.php"><?php echo '写文章'; ?></a></p>
+		                            <?php endif; ?>
+                                    <p><a href="<?php echo wp_logout_url('http://localhost/books/'); ?>"><?php echo '退出'; ?></a></p>
+	                            <?php else : ?>
+                                    <p><a href="http://localhost/books/wp-login.php"><?php echo '登录'; ?></a></p>
+                                    <p><a href="http://localhost/books/wp-login.php?action=register"><?php echo '注册'; ?></a></p>
+	                            <?php endif; ?>
+                                <?php
+/*                                    if ( is_user_logged_in() ) :
+                                        if ( in_array('administrator', $current_user->roles)) :
+                                            echo "写文章";
+                                        endif;
+                                        echo '退出';
+                                    else :
+                                        echo '登录';
+                                        echo '注册';
+                                    endif;
+                                */?>
+<!--                                <p>写文章</p>-->
+<!--                                <p>注册</p>-->
+<!--                                <p>登录</p>-->
+<!--                                <p>退出</p>-->
+                            </div>
+                        </div>
+                    </div>
+
 				</nav><!-- #site-navigation -->
 			</div><!-- #navbar -->
 		</header><!-- #masthead -->
